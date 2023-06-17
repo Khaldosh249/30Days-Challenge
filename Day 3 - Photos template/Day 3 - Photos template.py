bbf11898a25b4a86c9a6e34c5adcd0ac7d683313
Khaldosh249 <khalid.wad.rashad@gmail.com>
2023-06-17 14:45:51 +0200

    Last

--- Day 3 - Photos template/Day 3 - Photos template.py
+++ Day 3 - Photos template/Day 3 - Photos template.py
@@ -1,19 +1,7 @@
-from PIL import Image, ImageDraw, ImageFont
-
+from PIL import Image
 import os
 
-##############################################################################################
 
-print("""
-  ______ _   _______ ______ _____ _    _ 
- |  ____| | |__   __|  ____/ ____| |  | |
- | |__  | |    | |  | |__ | |    | |__| |
- |  __| | |    | |  |  __|| |    |  __  |
- | |____| |____| |  | |___| |____| |  | |
- |______|______|_|  |______\_____|_|  |_| 
-""")
-
-print("\n","\n","\n","\n","\n")
 
 print("""
   ____          _  ___           _ _     _   _____           _               _ 
@@ -25,30 +13,61 @@ print("""
          __/ |                                                                 
         |___/                                                                  
 """)
-temd = 'tem.png' # Template
-c = 1
+
+
+tem_name = 'tem.png' # Template Name
+
+c = 1 
 
 try:
     os.mkdir(os.getcwd() + "\Done")
 except FileExistsError:
     pass
 
-tem = Image.open(temd)
-width2, height2 = tem.size
+tem = Image.open(tem_name)
+width, height = tem.size
+
 
 for x in os.listdir():
 
-    if x[-4:] == ".jpg" and not x == temd:
+    if x[-4:] == ".jpg" and not x == tem_name:
+        try:
+            saved_path = 'Done/' + str(c) + ".jpg"  # save dir
+            pic = Image.open(x)
+            pic = Image.Image.resize(pic,(width, height))
+            pic.paste(tem, (0, 0) , tem)
+            pic.save(saved_path)
+            c = c + 1
+            pic.close()
+        except:
+            pass 
 
-        qrimg = 'Done/' + str(c) + ".jpg"  # save dir
-        pic = Image.open(x)
-        
-        pic = Image.Image.resize(pic,(width2, height2))
-        pic.paste(tem, (0, 0) , tem)
-        pic.save(qrimg)
-        c = c + 1
-
-        pic.close()
 
 
+readme = open("readme.txt" , "w+")
+
+readme.writelines(f"""
+{c-1} photos were placed in the template
+
+  ____          _  ___           _ _     _   _____           _               _ 
+ |  _ \        | |/ / |         | (_)   | | |  __ \         | |             | |
+ | |_) |_   _  | ' /| |__   __ _| |_  __| | | |__) |__ _ ___| |__   __ _  __| |
+ |  _ <| | | | |  < | '_ \ / _` | | |/ _` | |  _  // _` / __| '_ \ / _` |/ _` |
+ | |_) | |_| | | . \| | | | (_| | | | (_| | | | \ \ (_| \__ \ | | | (_| | (_| |
+ |____/ \__, | |_|\_\_| |_|\__,_|_|_|\__,_| |_|  \_\__,_|___/_| |_|\__,_|\__,_|
+         __/ |                                                                 
+        |___/                                                                  
+
+
+Khalid's social media links:
+https://www.linkedin.com/in/khalid-rashad-6560b7202
+https://www.facebook.com/khaldosh
+
+ELTECH's social media links:
+https://www.facebook.com/ELTECH.sd
+
+""")
+
+
+readme.close()
 print("Finished")
